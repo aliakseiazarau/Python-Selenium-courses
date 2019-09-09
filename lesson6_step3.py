4abc508982547d3bd2e49d6a61371de4eb5b49d8
aliakseiazarau <aliaksei.azarau@startmatter.com>
2019-09-09 18:51:29 +0300

    Add first python files

---
+++ lesson6_step3.py
@@ -0,0 +1,128 @@
+from selenium import webdriver
+import time
+import math
+
+
+# link = "http://suninjuly.github.io/simple_form_find_task.html"
+#
+# try:
+#     browser = webdriver.Chrome()
+#     browser.get(link)
+#
+#     input1 = browser.find_element_by_tag_name("input")
+#     input1.send_keys("Ivan")
+#     input2 = browser.find_element_by_name("last_name")
+#     input2.send_keys("Petrov")
+#     input3 = browser.find_element_by_class_name("form-control.city")
+#     input3.send_keys("Smolensk")
+#     input4 = browser.find_element_by_id("country")
+#     input4.send_keys("Russia")
+#     button = browser.find_element_by_css_selector("button.btn")
+#     button.click()
+
+# link = "http://suninjuly.github.io/find_link_text"
+
+# try:
+#     browser = webdriver.Chrome()
+#     browser.get(link)
+#     time.sleep(5)
+#     link1 = browser.find_element_by_link_text("224592")
+#     link1.click()
+#
+#     input1 = browser.find_element_by_tag_name("input")
+#     input1.send_keys("Ivan")
+#     input2 = browser.find_element_by_name("last_name")
+#     input2.send_keys("Petrov")
+#     input3 = browser.find_element_by_class_name("form-control.city")
+#     input3.send_keys("Smolensk")
+#     input4 = browser.find_element_by_id("country")
+#     input4.send_keys("Russia")
+#     button = browser.find_element_by_css_selector("button.btn")
+#     button.click()
+
+#
+# try:
+#     browser = webdriver.Chrome()
+#     browser.get("http://suninjuly.github.io/huge_form.html")
+#     elements = browser.find_elements_by_tag_name("input")
+#     for element in elements:
+#        element.send_keys("Pipi")
+#
+#     button = browser.find_element_by_css_selector("button.btn")
+#     button.click()
+
+
+# link = "http://suninjuly.github.io/find_xpath_form"
+#
+# try:
+#     browser = webdriver.Chrome()
+#     browser.get(link)
+#
+#     input1 = browser.find_element_by_tag_name("input")
+#     input1.send_keys("Ivan")
+#     input2 = browser.find_element_by_name("last_name")
+#     input2.send_keys("Petrov")
+#     input3 = browser.find_element_by_class_name("form-control.city")
+#     input3.send_keys("Smolensk")
+#     input4 = browser.find_element_by_id("country")
+#     input4.send_keys("Russia")
+#     button = browser.find_element_by_xpath('//button[text()="Submit"]')
+#     button.click()
+
+# try:
+#     link = "http://suninjuly.github.io/registration1.html"
+#     browser = webdriver.Chrome()
+#     browser.get(link)
+#
+#     # Ваш код, который заполняет обязательные поля
+#     input1 = browser.find_element_by_css_selector(".first_block .first")
+#     input1.send_keys("Pipi")
+#     input2 = browser.find_element_by_css_selector(".first_block .second")
+#     input2.send_keys("SyuSyu")
+#     input3 = browser.find_element_by_css_selector(".first_block .third")
+#     input3.send_keys("Pipisyusyu@mail.comfwd")
+#
+#     # Отправляем заполненную форму
+#     button = browser.find_element_by_css_selector("button.btn")
+#     button.click()
+#
+#     # Проверяем, что смогли зарегистрироваться
+#     # ждем загрузки страницы
+#     time.sleep(1)
+#
+#     # находим элемент, содержащий текст
+#     welcome_text_elt = browser.find_element_by_tag_name("h1")
+#     # записываем в переменную welcome_text текст из элемента welcome_text_elt
+#     welcome_text = welcome_text_elt.text
+#
+#     # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+#     assert "Congratulations! You have successfully registered!" == welcome_text
+
+link = "http://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    input = browser.find_element_by_id("answer")
+    input.send_keys(y)
+
+    checkbox = browser.find_element_by_css_selector("[for='robotCheckbox']")
+
+
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
