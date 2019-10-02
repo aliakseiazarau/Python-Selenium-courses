360f32b1ec137da60f7f2058c925ffaffe66a810
aliakseiazarau <aliaksei.azarau@startmatter.com>
2019-10-02 19:20:08 +0300

    Add tests with fixtures

---
+++ test_UFO_3_6.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import time
+import math
+import pytest
+
+
+@pytest.fixture(scope="session")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+links = ["https://stepik.org/lesson/236895/step/1",
+         "https://stepik.org/lesson/236896/step/1",
+         "https://stepik.org/lesson/236897/step/1",
+         "https://stepik.org/lesson/236898/step/1",
+         "https://stepik.org/lesson/236899/step/1",
+         "https://stepik.org/lesson/236903/step/1",
+         "https://stepik.org/lesson/236904/step/1",
+         "https://stepik.org/lesson/236905/step/1"]
+
+
+@pytest.mark.parametrize('url', links)
+def test_ufo_message(browser, url):
+    browser.get(url)
+    answer = math.log(int(time.time()))
+    time.sleep(3)
+    pole = browser.find_element_by_tag_name("textarea")
+    pole.send_keys(str(answer))
+    browser.find_element_by_css_selector(".submit-submission ").click()
+    time.sleep(3)
+    compare = browser.find_element_by_class_name("smart-hints__hint").text
+    assert compare in "Correct!"
+
+
